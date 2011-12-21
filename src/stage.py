d89d7a4dfaedabd6fa75215cae4dfbc7b6a18557
Alex Beal <alexlbeal@gmail.com>
2011-12-21 19:34:48 +0400

    Added abstract version of setInput()
    1) I added and abstract version of the setInput() method to the stage.py
    class. Uncessary, but it will help me remember.
    2) Added a new exception that gets thrown if one of the abstract methods
    in the stage class gets invoked.

--- src/stage.py
+++ src/stage.py
@@ -6,12 +6,18 @@ class Stage(object):
                      'is_bound_method', 'is_unbound_method']
 
     # Public Methods:
+    def setInput(self):
+        """Sets the input to the stage and prepares the class for the executing
+           the do() method.
+        """
+        raise AbstractMethod('Attempting to call abstract setInput()')
+
     def do(self):
         """This method needs to be overridden by each stage that's derived from it.
            It should carry out all the processes necessary to complete that stage of
            the compile.
         """
-        pass
+        raise AbstractMethod('Attempting to call abstract do()')
 
     # Private Methods:
     def _genName(self, prefix):
@@ -20,3 +26,9 @@ class Stage(object):
         while True:
             yield prefix + str(counter)
             counter += 1
+
+class AbstractMethod(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
