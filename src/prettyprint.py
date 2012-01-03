ea04b94203f12087dbb2e5bbe015f29a6bff9adf
Alex Beal <alexlbeal@gmail.com>
2012-01-04 01:31:17 +0400

    PrettyPrint breaks lines down more intelligently.
    1) PrettyPrint will now break a node down into its children if its
    string representation is longer than a set constant (currently 50
    characters).
    2) That allowed me to remove lots of duplicate functions.

--- src/prettyprint.py
+++ src/prettyprint.py
@@ -7,6 +7,8 @@ class PrettyPrint(astvisitor.ASTVisitor):
 
     # Configure how many spaces per indent
     _INDENT_NUM = 2
+    # How many characters in a line before it's broken down.
+    _LINE_LENGTH = 50
 
     def prettyPrint(self, ast):
         return self.visit(ast, 0, "")
@@ -21,12 +23,21 @@ class PrettyPrint(astvisitor.ASTVisitor):
         return str + ")\n"
 
     def default(self, ast, indent, ast_str):
+        """The default method for printing a node.
+           If the string representation for the current node (ast) is longer
+           than _LINE_LENGTH the node gets broken down into parts (its
+           children).
+        """
+        if len(str(ast)) < self._LINE_LENGTH:
+            return self._visit_OneLine(ast, indent, ast_str)
+
         ast_str = self._appendIndents(ast_str, indent)
         ast_str = self._appendNodeName(ast_str, ast)
         for child in ast.getChildNodes():
             ast_str = self.visit(child, indent + 1, ast_str)
         ast_str = self._appendIndents(ast_str, indent)
         ast_str = self._appendCloseParen(ast_str)
+
         return ast_str
 
     def _visit_OneLine(self, ast, indent, ast_str):
@@ -34,16 +45,4 @@ class PrettyPrint(astvisitor.ASTVisitor):
         ast_str= self._appendIndents(ast_str, indent)
         ast_str += str(ast) + "\n"
         return ast_str
-
-    def visit_Const(self, *args):
-        return self._visit_OneLine(*args)
-
-    def visit_Name(self, *args):
-        return self._visit_OneLine(*args)
-
-    def visit_AssName(self, *args):
-        return self._visit_OneLine(*args)
-
-    def visit_Add(self, *args):
-        return self._visit_OneLine(*args)
 
